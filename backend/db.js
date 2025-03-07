f6236ee959c9a1738033a011407a95e0ad20fea1
dongbinkoo <rnehdqlsdbxbqm@gmail.com>
2025-03-08 00:46:55 +0900

    db 보안관련 설정 변경

--- backend/db.js
+++ backend/db.js
@@ -5,8 +5,8 @@ const db = mysql
     .createPool({
         host: process.env.DB_HOST || 'localhost',
         user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASS || 'ckawpwk0165!', // ⚠️ 비밀번호는 .env에서 관리하는 것이 더 안전
-        database: process.env.DB_NAME || 'my_fonts_db',
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
         port: process.env.DB_PORT || 3306,
         waitForConnections: true,
         connectionLimit: 100,
