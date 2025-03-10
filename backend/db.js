ae1260cbe2dd983f124defb675ae9d56a5544929
dongbinkoo <rnehdqlsdbxbqm@gmail.com>
2025-03-10 16:19:13 +0900

    Updated db.js and frontend changes

--- backend/db.js
+++ backend/db.js
@@ -5,8 +5,8 @@ const db = mysql
     .createPool({
         host: process.env.DB_HOST || 'localhost',
         user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
+        password: process.env.DB_PASS || 'ckawpwk0165!',
+        database: process.env.DB_NAME || 'my_fonts_db',
         port: process.env.DB_PORT || 3306,
         waitForConnections: true,
         connectionLimit: 100,
