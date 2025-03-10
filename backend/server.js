ac88e6af67500d5e97806a19686cfc92008e1e59
dongbinkoo <rnehdqlsdbxbqm@gmail.com>
2025-03-10 20:45:07 +0900

    백엔드 서버코드 수정

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,8 @@ const db = require('./db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-    cors({
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true,
-    })
-);
+app.use(cors({ origin: '*' })); // 모든 도메인 허용
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
